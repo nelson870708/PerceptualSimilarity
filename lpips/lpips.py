b447c8066468915fc4110977c3d5fd1778e7f5e8
nelson870708 <nelson870708@gmail.com>
2021-09-01 22:13:15 +0800

    rearrange code style

--- lpips/lpips.py
+++ lpips/lpips.py
@@ -14,7 +14,6 @@ def spatial_average(in_tens, keepdim=True):
 
 
 def upsample(in_tens, out_HW=(64, 64)):  # assumes scale factor is same for H and W
-    in_H, in_W = in_tens.shape[2], in_tens.shape[3]
     return nn.Upsample(size=out_HW, mode='bilinear', align_corners=False)(in_tens)
 
 
